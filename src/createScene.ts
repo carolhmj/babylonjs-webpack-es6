44cdcb09c410f17f28cd6aade9c56be283761b8e
Carolina Herbster <carolhmj@gmail.com>
2023-12-20 10:17:32 -0300

    rollback to loadmodelandenv scene

--- src/createScene.ts
+++ src/createScene.ts
@@ -4,10 +4,10 @@ import type { Scene } from "@babylonjs/core/scene";
 // Change this import to check other scenes
 //import { DefaultSceneWithTexture } from "./scenes/defaultWithTexture";
 import { LoadModelAndEnvScene } from "./scenes/loadModelAndEnv";
-import { FresnelShaderScene } from "./scenes/fresnelShader";
-import {NavigationMeshRecast} from "./scenes/navigationMeshRecast";
+// import { FresnelShaderScene } from "./scenes/fresnelShader";
+// import {NavigationMeshRecast} from "./scenes/navigationMeshRecast";
 //import scene from "./scenes/physicsWithAmmo";
-import scene from "./scenes/physicsWithHavok";
+// import scene from "./scenes/physicsWithHavok";
 
 export interface CreateSceneClass {
     createScene: (engine: Engine, canvas: HTMLCanvasElement) => Promise<Scene>;
@@ -20,8 +20,8 @@ export interface CreateSceneModule {
 
 export const getSceneModule = (): CreateSceneClass => {
     // return new DefaultSceneWithTexture();
-    //return new LoadModelAndEnvScene();
+    return new LoadModelAndEnvScene();
     //return new FresnelShaderScene();
     //return new NavigationMeshRecast();
-    return scene;
+    // return scene;
 }
